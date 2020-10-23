4fd476a48eb70870fcc6eaf6c22232684787ba49
mmoreira <mmoreira@bsolus.pt>
2020-10-23 23:38:24 +0100

    Added class to business transaction.

--- app/Business/Transaction.php
+++ app/Business/Transaction.php
@@ -2,13 +2,65 @@
 
 namespace App\Business;
 
+use App\Business\Model\AccountInterface;
+use App\Business\Model\TransactionInterface;
+
 class Transaction
 {
-    public function execute
+    private $transaction;
+
+    public function __construct(TransactionInterface $transaction = null)
     {
+        $this->transaction = $transaction;
     }
 
-    public function retrieve
+    public function execute(TransactionInterface $transaction) : TransactionInterface
+    {
+        if (!$transaction->getStatus()) {
+            return $transaction;
+        }
+
+        return $transaction;
+    }
+
+    public function validateAvailability(TransactionInterface $transaction) : bool
+    {
+        $payer = $transaction->getWalletPayer();
+        $payee = $transaction->getWalletPayee();
+
+        if ($this->checkTypeAccount($transaction)) {
+            return false;
+        }
+
+        if (!$this->checkAmoutAvailable($transaction)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function checkAmoutAvailable(TransactionInterface $transaction)
+    {
+        $payer = $transaction->getWalletPayer();
+        return $payer->getAmount() > $transaction->getValue();
+    }
+
+    protected function checkTypeAccount(TransactionInterface $transaction)
+    {
+        $account = $transaction
+            ->getWalletPayer()
+            ->getAccount()
+        ;
+
+        return $account->getType() == AccountInterface::TYPE_BUSINESS;
+    }
+
+    public function getTransaction()
+    {
+        return $this->transaction;
+    }
+
+    public function retrieve()
     {
     }
 }
